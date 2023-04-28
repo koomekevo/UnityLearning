0a3f9c23622eedab2f9c1a46cae91b231d23c3c7
koomekevo <koome.kevo@gurucrafts.net>
2023-04-28 17:04:03 +0300

    Add BoxMover.cs Script with Speed and Strafe_speed variables

--- Assets/Scripts/BoxMover.cs
+++ Assets/Scripts/BoxMover.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class BoxMover : MonoBehaviour
 {
+    public float speed = 0;
+    public float strafe_speed = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,7 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("walk speed is " + speed);
+        Debug.Log("strafe speed is " + strafe_speed);
     }
 }
